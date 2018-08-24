41713808d2bb15701ec8bb61a5321981a128f0d2
Wolfy-J <wolfy.jd@gmail.com>
2018-08-24 12:15:13 +0300

    first tests

--- tests/Exceptions/HighlighterTest.php
+++ tests/Exceptions/HighlighterTest.php
@@ -19,32 +19,90 @@ class HighlighterTest extends TestCase
     public function testPlainHighlighter()
     {
         $highlighter = new Highlighter(new PlainStyle());
-        $result = $highlighter->highlight(file_get_contents(__FILE__));
 
-        $this->assertContains("HighlighterTest", $result);
+        $this->assertContains("HighlighterTest", $highlighter->highlight(file_get_contents(__FILE__)));
     }
 
     public function testHtmlHighlighter()
     {
         $highlighter = new Highlighter(new HtmlStyle(HtmlStyle::DEFAULT));
-        $result = $highlighter->highlight(file_get_contents(__FILE__));
 
-        $this->assertContains("HighlighterTest", $result);
+        $this->assertContains("HighlighterTest", $highlighter->highlight(file_get_contents(__FILE__)));
     }
 
     public function testInvertedHtmlHighlighter()
     {
         $highlighter = new Highlighter(new HtmlStyle(HtmlStyle::INVERTED));
-        $result = $highlighter->highlight(file_get_contents(__FILE__));
 
-        $this->assertContains("HighlighterTest", $result);
+        $this->assertContains("HighlighterTest", $highlighter->highlight(file_get_contents(__FILE__)));
+
     }
 
     public function testConsoleHighlighter()
     {
         $highlighter = new Highlighter(new ConsoleStyle());
-        $result = $highlighter->highlight(file_get_contents(__FILE__));
 
-        $this->assertContains("HighlighterTest", $result);
+        $this->assertContains("HighlighterTest", $highlighter->highlight(file_get_contents(__FILE__)));
+    }
+
+    public function testPlainHighlighterLines()
+    {
+        $highlighter = new Highlighter(new PlainStyle());
+
+        $this->assertContains(
+            "HighlighterTest",
+            $highlighter->highlightLines(file_get_contents(__FILE__), 17)
+        );
+    }
+
+    public function testHtmlHighlighterLines()
+    {
+        $highlighter = new Highlighter(new HtmlStyle(HtmlStyle::DEFAULT));
+
+        $this->assertContains(
+            "HighlighterTest",
+            $highlighter->highlightLines(file_get_contents(__FILE__), 17)
+        );
+    }
+
+    public function testInvertedHtmlHighlighterLines()
+    {
+        $highlighter = new Highlighter(new HtmlStyle(HtmlStyle::INVERTED));
+
+        $this->assertContains(
+            "HighlighterTest",
+            $highlighter->highlightLines(file_get_contents(__FILE__), 17)
+        );
+
+    }
+
+    public function testConsoleHighlighterLines()
+    {
+        $highlighter = new Highlighter(new ConsoleStyle());
+
+        $this->assertContains(
+            "HighlighterTest",
+            $highlighter->highlightLines(file_get_contents(__FILE__), 17)
+        );
+    }
+
+    public function testCountLines()
+    {
+        $highlighter = new Highlighter(new PlainStyle());
+
+        $this->assertCount(
+            1,
+            explode("\n", trim($highlighter->highlightLines(file_get_contents(__FILE__), 0, 1), "\n"))
+        );
+
+        $this->assertCount(
+            2,
+            explode("\n", trim($highlighter->highlightLines(file_get_contents(__FILE__), 1, 1), "\n"))
+        );
+
+        $this->assertCount(
+            3,
+            explode("\n", trim($highlighter->highlightLines(file_get_contents(__FILE__), 2, 1), "\n"))
+        );
     }
 }
