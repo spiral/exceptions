ebd64eaf502538ec27b091d2bc318d0435fb5299
Wolfy-J <wolfy.jd@gmail.com>
2019-11-08 12:14:07 +0300

    - fix the folding states

--- views/partials/stacktrace.php
+++ views/partials/stacktrace.php
@@ -1,11 +1,16 @@
 <?php
 /**
+ * @var array                           $stacktrace
  * @var Throwable                       $exception
  * @var \Spiral\Exceptions\ValueWrapper $valueWrapper
  * @var \Spiral\Exceptions\Highlighter  $highlighter
  * @var bool                            $showSource
  */
-foreach ($stacktrace as $trace) {
+$vendorDir = (new ReflectionClass(\Composer\Autoload\ClassLoader::class))->getFileName();
+$vendorDir = dirname(dirname($vendorDir));
+$vendorID = null;
+$vendorCount = 0;
+foreach ($stacktrace as $index => $trace) {
     $args = [];
     if (isset($trace['args'])) {
         $args = $valueWrapper->wrap($trace['args']);
@@ -29,17 +34,44 @@ foreach ($stacktrace as $trace) {
         </div>
         <?php
         continue;
-    } ?>
+    }
+
+    $isVendor = strpos($trace['file'], $vendorDir) === 0 && $index > 1;
+    if ($isVendor) {
+        if ($vendorID === null) {
+            $vendorID = $index;
+            $vendorCount++;
+            echo sprintf('<span id="hidden-trace-%s" style="display: none;">', $vendorID);
+        } else {
+            $vendorCount++;
+        }
+    } elseif ($vendorID !== null) {
+        echo '</span>';
+        echo sprintf(
+            '<div id="%s" class="container" style="cursor: pointer;" onclick="toggle(\'%s\'); toggle(\'%s\');">&plus; %s vendor frame(s)...</div>',
+            'toggle-trace-' . $vendorID,
+            'toggle-trace-' . $vendorID,
+            'hidden-trace-' . $vendorID,
+            $vendorCount
+        );
+        $vendorID = null;
+        $vendorCount = 0;
+    }
+    ?>
     <div class="container">
         <div class="location">
             <?= $function ?>(<span class="arguments"><?= join(', ', $args) ?></span>)<br/>
             <em>In&nbsp;<?= $trace['file'] ?>&nbsp;at&nbsp;<strong>line <?= $trace['line'] ?></strong></em>
         </div>
-        <?php if ($showSource) : ?>
+        <?php if ($showSource && file_exists($trace['file'])) : ?>
             <div class="lines">
                 <?= $highlighter->highlightLines(file_get_contents($trace['file']), $trace['line']) ?>
             </div>
         <?php endif; ?>
     </div>
     <?php
+}
+
+if ($vendorID !== null) {
+    echo '</span>';
 }
